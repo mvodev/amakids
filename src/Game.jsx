46ab6f091aa221be0ad379084e619f5da9be2641
Vadim Manushin <vadimmanushin@yandex.ru>
2022-07-25 17:53:15 +0300

    added header into main page

--- src/Game.jsx
+++ src/Game.jsx
@@ -7,6 +7,7 @@ const Game = () => {
   return (
     <div className="game">
       <h1 className="game__header">Тестовое задание для AMAkids</h1>
+      <h2 className="game__header">Игра Лабиринт</h2>
       <GameField fieldSize={3}/>
     </div>
   );
